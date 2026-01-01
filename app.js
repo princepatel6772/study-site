2d6d0a77f9887846e5f669e0823fc0bce4dd32f2
princepatel6772 <princepatel6772@gmail.com>
2026-01-01 14:37:46 +0530

    Remove uploadFile function and clean up code

--- app.js
+++ app.js
@@ -12,20 +12,6 @@ function login() {
   document.getElementById("mainBox").classList.remove("hidden");
 }
 
-function uploadFile() {
-  const fileInput = document.getElementById("fileInput");
-  if (!fileInput.files.length) return alert("Select file");
-
-  const file = fileInput.files[0];
-  const list = document.getElementById("fileList");
-
-  const li = document.createElement("li");
-  li.innerHTML = `
-    ${file.name}
-    <button onclick="download('${file.name}')">Download</button>
-  `;
-  list.appendChild(li);
-}
 
 function download(fileName) {
   let logs = JSON.parse(localStorage.getItem("logs") || "[]");
@@ -33,4 +19,5 @@ function download(fileName) {
   localStorage.setItem("logs", JSON.stringify(logs));
   alert("Download recorded (demo)");
 }
+
 
